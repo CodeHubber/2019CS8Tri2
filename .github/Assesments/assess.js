98e32af22ca6640e6d5a9f548695c1770227a5cc
CodeHubber <gflickinger24@nscds.org>
2020-02-07 15:25:09 +0000

    hello

--- .github/Assesments/assess.js
+++ .github/Assesments/assess.js
@@ -1,5 +1,12 @@
 $(".type1").click(
     function(){
-    alert("Hello")
+        alert("Hello")
     }
 );
+
+$(".type2").click(
+    function(){
+       var blah = $(this).css("background-color");
+       $('#bottom').css("background-color",blah);
+    }
+ );
